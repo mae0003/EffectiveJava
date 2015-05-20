6f1b0b7943ec51278f6098e6e44ce534e73a526f
mae0003 <mae0003@gmail.com>
2015-05-20 19:08:19 +0900

    add javadoc comment.

--- src/main/java/Ej57/Ej57.java
+++ src/main/java/Ej57/Ej57.java
@@ -1,14 +1,21 @@
 package Ej57;
+
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class Ej57 {
 
+	/**
+	 * パラメータの文字列が数値に変換できるか判定します。
+	 * 
+	 * @param value
+	 *            検査文字列
+	 * @return 数値に変換できる場合 true
+	 */
 	public boolean validateNumber(String value) {
 		return NumberUtils.isNumber(value);
 	}
 
 	public boolean validateNumber1(String value) {
-
 		return false;
 	}
 
